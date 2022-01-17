9d8a4ef36ff1dac896b2a0e2833ce21d3e632160
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-01-17 16:04:18 -0300

    REQUISITO 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,32 @@
     - average([1, '2']) // Retorno: undefined;
 */
 // Modificação pra meu primeiro commit
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    const type = typeof array[i];
+    if (type !== 'number') {
+      return undefined;
+    } sum += array[i];
+  }
+  const averageEnd = Math.round(sum / array.length);
+  return averageEnd;
+};
 
 module.exports = average;
+
+// 15 - constante recebe um function com os arrays
+// 16 criei uma variavel que armazenará valores da minha estrutura de repetição (soma dos valores das posições dos arrays)
+// 17 se o tamanho do meu array da function for estreitamente igual a 0
+// 18 retornará undefined
+
+// 20 uma estrutura de repetição que irá percorré cada possição do meu array
+// 21 criei uma constante de armazanerá qual o valor primitivo das posições do meu array
+// 22 se o tipo e o valor de type for diferente de 'number'
+// 23 será retornado undefined 
+// 24 soma ao valor da variavel 'sum' a soma das posiçoes do array
+// 25 cria constante que arrendorá para o inteiro mais proximo a divisão das somas das posições dos arrays pelo quantidade de posições
+// 26 retorna a constante
