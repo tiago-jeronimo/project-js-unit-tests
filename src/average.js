1033983eafe844f73b02e5513e4af65aa8fa38e9
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-01-17 14:46:56 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/average.js
+++ src/average.js
@@ -11,7 +11,7 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
+// Modificação pra meu primeiro commit
 const average = () => {};
 
 module.exports = average;
